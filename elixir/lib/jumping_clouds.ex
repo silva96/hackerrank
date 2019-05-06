3a3c9f100e0c8c5ccbfce6fe046d71cf11695ed1
Benjamín Silva <silva96@gmail.com>
2019-05-06 10:56:15 -0400

    add jumping_clouds problem

--- elixir/lib/jumping_clouds.ex
+++ elixir/lib/jumping_clouds.ex
@@ -1,3 +1,28 @@
 defmodule JumpingClouds do
-  
+  def jump(clouds, length, is_first_step \\ false) do
+    if(length(clouds) == 1) do
+      1 # this is the last cloud, we always jump on it.
+    else
+      if List.first(clouds) == "0" do
+        one_step = clouds |> Enum.slice(1, length)
+        two_steps = clouds |> Enum.slice(2, length)
+
+        min = Enum.min([
+          jump(one_step, length),
+          jump(two_steps, length)
+        ])
+        add_step = if is_first_step, do: 0, else: 1
+        min + add_step
+      else
+        length # so it's never better than the other step.
+      end
+    end
+  end
+
+  def process do
+    length = IO.gets("Number clouds\n") |> String.trim() |> String.to_integer()
+    clouds_array = IO.gets("Enter the clouds\n") |> to_string() |> String.trim() |> String.split()
+    result = JumpingClouds.jump(clouds_array, length, true)
+    IO.puts(result)
+  end
 end
--- elixir/test/jumping_clouds_test.exs
+++ elixir/test/jumping_clouds_test.exs
@@ -2,7 +2,21 @@ defmodule JumpingCloudsTest do
   use ExUnit.Case
   doctest JumpingClouds
 
-  test " " do
-    # assert 
+  test "It jumps correctly on the clouds of Sample Input 0" do
+    clouds = "0 0 1 0 0 1 0"
+    clouds_array = String.split(clouds)
+    assert JumpingClouds.jump(clouds_array, length(clouds_array), true) == 4
+  end
+
+  test "It jumps correctly on the clouds of Sample Input 1" do
+    clouds = "0 0 0 0 1 0"
+    clouds_array = String.split(clouds)
+    assert JumpingClouds.jump(clouds_array, length(clouds_array), true) == 3
+  end
+
+  test "It jumps correctly on the clouds of Sample Input 2" do
+    clouds = "0 0 0 1 0 0"
+    clouds_array = String.split(clouds)
+    assert JumpingClouds.jump(clouds_array, length(clouds_array), true) == 3
   end
 end
